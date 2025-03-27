579189047a11febe75a5e27521447f1c5ab41d71
nmasalkin <nmasalkin96@yandex.ru>
2025-03-28 02:42:47 +0300

    Add site sample

---
+++ src/main/java/ru/vsu/cs/masalkin/nmasalkin/NmasalkinApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.masalkin.nmasalkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmasalkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NmasalkinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vsu/cs/masalkin/nmasalkin/NmasalkinApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.masalkin.nmasalkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmasalkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
